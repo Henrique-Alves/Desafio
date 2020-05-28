7555d2766c72628e9ecfd193811026cf2ef40bd9
Henrique da Silva Alves <henriquehdsa@gmail.com>
2020-05-28 17:41:53 -0300

    atualização dos cards

--- src/app/arquivoJson.js
+++ src/app/arquivoJson.js
@@ -7,7 +7,7 @@ const listaGame = {
             tipo: "RPG",
             nota: "9"
         },
-
+           
         {
             nome: "CS",
             img: "https://images.uncyc.org/pt/thumb/a/a0/Csgraphicnovel.jpg/300px-Csgraphicnovel.jpg",
@@ -15,7 +15,7 @@ const listaGame = {
             tipo: "acão",
             nota: "9"
         },
-
+    
         {
             nome: "battlefield 5",
             img: "https://thumbs.gfycat.com/FittingFarCommongonolek-size_restricted.gif",
@@ -23,16 +23,57 @@ const listaGame = {
             tipo: "cão",
             nota: "9"
         },
-
+          
         {
             nome: "call of duty",
             img: "https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2019/11/call-of-duty-mobile-950x606.jpg",
             anoLancamento: "2003",
             tipo: "acão",
             nota: "7"
-        }
-
+        },
+    
+        {
+            nome: "Assassin's Creed",
+            img: "https://s2.glbimg.com/x5dqQybuuq4-kwc-wNuHTeH-r64=/695x0/s.glbimg.com/po/tt2/f/original/2016/11/30/assassins-creed-3-gratis-uplay-ubisoft-30-anos.jpg",
+            anoLancamento: "20012",
+            tipo: "acão e aventura",
+            nota: "7"
+        },
+    
+        {
+            nome: "call of duty",
+            img: "https://cdn.awsli.com.br/239/239279/produto/7052446/painel-para-decoracao-de-festa-infantil-watch-dogs-bc2d73dd.jpg",
+            anoLancamento: "20014",
+            tipo: "ação e aventura",
+            nota: "8"
+        },
     ]
+
+    // games2: [
+    //     {
+    //         nome: "call of duty",
+    //         img: "https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2019/11/call-of-duty-mobile-950x606.jpg",
+    //         anoLancamento: "2003",
+    //         tipo: "acão",
+    //         nota: "7"
+    //     },
+
+    //     {
+    //         nome: "Assassin's Creed",
+    //         img: "https://s2.glbimg.com/x5dqQybuuq4-kwc-wNuHTeH-r64=/695x0/s.glbimg.com/po/tt2/f/original/2016/11/30/assassins-creed-3-gratis-uplay-ubisoft-30-anos.jpg",
+    //         anoLancamento: "20012",
+    //         tipo: "acão e aventura",
+    //         nota: "7"
+    //     },
+
+    //     {
+    //         nome: "call of duty",
+    //         img: "https://cdn.awsli.com.br/239/239279/produto/7052446/painel-para-decoracao-de-festa-infantil-watch-dogs-bc2d73dd.jpg",
+    //         anoLancamento: "20014",
+    //         tipo: "ação e aventura",
+    //         nota: "8"
+    //     }
+    // ]
 }
 // console.log(listaGame);
 export default listaGame;
--- src/app/card.js
+++ src/app/card.js
@@ -2,21 +2,49 @@ import React from 'react';
 import listaGame from './arquivoJson.js';
 
 const Card = () => {
-    return(
-        listaGame.games.map((element, index) => (
-            <div className="card">
-              <img src={element.img}  className="card-img-top" alt="..."/><p>Henrique</p>
-              <div className="card-body">
-                <h5 className="card-title">
-                {"Jogo: "+ element.nome} 
-                {" ,Ano de Lançamento: "+ element.anoLancamento} 
-                {" ,Estilo do Jogo: "+ element.tipo}
-                {" ,Nota do jogo: "+ element.nota}
-                </h5>
-                <p className="card-text">Jogos que mais joguei de 2017 a 2020</p>
-              </div>
-            </div>     
-           ))    
+    return (
+        listaGame.games.map((element, index) => (       
+            <div className="row row-cols-1 row-cols-md-3">
+                <div className="col mb-4">
+                    <div className="card h-100">
+                        <img src={element.img} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {"Jogo: " + element.nome}
+                                {", Ano de Lançamento: " + element.anoLancamento}
+                                {", Estilo do Jogo: " + element.tipo}
+                                {", Nota do jogo: " + element.nota}
+                            </h5>
+                            <p className="card-text">.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card h-100">
+                        <img src={element.img} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{"Jogo: " + element.nome}
+                                {", Ano de Lançamento: " + element.anoLancamento}
+                                {", Estilo do Jogo: " + element.tipo}
+                                {", Nota do jogo: " + element.nota}</h5>
+                            <p className="card-text">This is a short card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card h-100">
+                        <img src={element.img} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{"Jogo: " + element.nome}
+                                {", Ano de Lançamento: " + element.anoLancamento}
+                                {", Estilo do Jogo: " + element.tipo}
+                                {", Nota do jogo: " + element.nota}</h5>
+                            <p className="card-text"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ))
     );
 }
 
